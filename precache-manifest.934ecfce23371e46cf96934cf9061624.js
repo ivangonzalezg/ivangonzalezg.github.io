77ea764457475dd48cf722c1657f048921085611
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2020-05-23 03:49:03 -0500

    Updates

---
+++ precache-manifest.934ecfce23371e46cf96934cf9061624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9453a6ea92e3ef53980136e5eef5ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5da7dcfc6eaeb0000867",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ee1804cdacfe73bfbd27",
+    "url": "/static/css/main.3044c238.chunk.css"
+  },
+  {
+    "revision": "5da7dcfc6eaeb0000867",
+    "url": "/static/js/2.9938e3a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9938e3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1804cdacfe73bfbd27",
+    "url": "/static/js/main.805e0ef7.chunk.js"
+  },
+  {
+    "revision": "0f3feded836a786f3d04",
+    "url": "/static/js/runtime-main.8fd90322.js"
+  }
+]);
