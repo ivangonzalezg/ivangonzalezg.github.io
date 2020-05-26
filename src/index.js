5973e37afa63fa54ed2b6d813c77ab2454fac68c
Ivan Gonzalez <ivangonzalezgrc@gmail.com>
2020-05-26 03:12:08 -0500

    Using Tim Baker template

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.scss";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
